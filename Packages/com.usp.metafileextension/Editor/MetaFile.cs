e448003c17585ebfd5271c9a5ac71853c543eed8
Joel Barba <joel.barba@unity3d.com>
2025-01-09 20:13:29 -0600

    added namespace

--- Packages/com.usp.metafileextension/Editor/MetaFile.cs
+++ Packages/com.usp.metafileextension/Editor/MetaFile.cs
@@ -5,30 +5,54 @@ using System.Text.Json;
 using UnityEngine;
 using UnityEditor;
 
-public class MetaFile
+namespace USP.MetaFileExtension
 {
-    #region Static Methods
-    public static T Read<T>(AssetImporter assetImporter, string key, Func<AssetImporter, T> factory = null)
+    public class MetaFile
     {
-        var jsonObjectMap = GetUserDataMap(assetImporter);
-
-        // Attempt to get the item associated with the key.
-        // If there is an item associated with the key, then:
-        if (jsonObjectMap.TryGetValue(key, out string userDataEntryJson))
+        #region Static Methods
+        public static T Read<T>(AssetImporter assetImporter, string key, Func<AssetImporter, T> factory = null)
         {
-            // Deserialize the user data entry from JSON.
-            T userDataEntry = JsonUtility.FromJson<T>(userDataEntryJson);
+            var jsonObjectMap = GetUserDataMap(assetImporter);
 
-            return userDataEntry;
+            // Attempt to get the item associated with the key.
+            // If there is an item associated with the key, then:
+            if (jsonObjectMap.TryGetValue(key, out string userDataEntryJson))
+            {
+                // Deserialize the user data entry from JSON.
+                T userDataEntry = JsonUtility.FromJson<T>(userDataEntryJson);
+
+                return userDataEntry;
+            }
+
+            // Otherwise, there are no items associated with the key.
+
+            // If there is a valid factory creation, then:
+            if (factory != null)
+            {
+                // Create a new instance of the user data based off the asset importer.
+                T userDataEntry = factory(assetImporter);
+
+                // Serialize the user data to JSON.
+                // Associate the JSON entry with the key.
+                jsonObjectMap[key] = JsonUtility.ToJson(userDataEntry);
+
+                // Serialize the map to JSON.
+                // Set the asset importer's user data.
+                assetImporter.userData = JsonSerializer.Serialize(jsonObjectMap);
+
+                // Return the new instance.
+                return userDataEntry;
+            }
+
+            // Otherwise, there is no factory valid factory creation.
+
+            // There is no item, so return a default value.
+            return default;
         }
 
-        // Otherwise, there are no items associated with the key.
-        
-        // If there is a valid factory creation, then:
-        if (factory != null)
+        public static void Write<T>(AssetImporter assetImporter, string key, T userDataEntry)
         {
-            // Create a new instance of the user data based off the asset importer.
-            T userDataEntry = factory(assetImporter);
+            var jsonObjectMap = GetUserDataMap(assetImporter);
 
             // Serialize the user data to JSON.
             // Associate the JSON entry with the key.
@@ -38,38 +62,17 @@ public class MetaFile
             // Set the asset importer's user data.
             assetImporter.userData = JsonSerializer.Serialize(jsonObjectMap);
 
-            // Return the new instance.
-            return userDataEntry;
+            assetImporter.SaveAndReimport();
         }
 
-        // Otherwise, there is no factory valid factory creation.
+        private static Dictionary<string, string> GetUserDataMap(AssetImporter assetImporter)
+        {
+            var userDataMapJson = string.IsNullOrEmpty(assetImporter.userData) ? "{}" : assetImporter.userData;
 
-        // There is no item, so return a default value.
-        return default;
+            var userDataMap = JsonSerializer.Deserialize<Dictionary<string, string>>(userDataMapJson);
+
+            return userDataMap; // ?? new Dictionary<string, string>();
+        }
+        #endregion
     }
-
-    public static void Write<T>(AssetImporter assetImporter, string key, T userDataEntry)
-    {
-        var jsonObjectMap = GetUserDataMap(assetImporter);
-
-        // Serialize the user data to JSON.
-        // Associate the JSON entry with the key.
-        jsonObjectMap[key] = JsonUtility.ToJson(userDataEntry);
-
-        // Serialize the map to JSON.
-        // Set the asset importer's user data.
-        assetImporter.userData = JsonSerializer.Serialize(jsonObjectMap);
-
-        assetImporter.SaveAndReimport();
-    }
-
-    private static Dictionary<string, string> GetUserDataMap(AssetImporter assetImporter)
-    {
-        var userDataMapJson = string.IsNullOrEmpty(assetImporter.userData) ? "{}" : assetImporter.userData;
-
-        var userDataMap = JsonSerializer.Deserialize<Dictionary<string, string>>(userDataMapJson);
-
-        return userDataMap; // ?? new Dictionary<string, string>();
-    }
-    #endregion
 }
